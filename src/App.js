d731aee435a88076ce2f6c73e043f731bbf620bf
manish465 <ms5978307@gmail.com>
2020-04-17 16:02:44 -0700

    added few components to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header/Header";
+import TabBar from "./components/TabBar/TabBar";
+import Banner from "./components/Banner/Banner";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-    return <div className='App'>Hello world</div>;
+    return (
+        <div className='App'>
+            <Header />
+            <TabBar />
+            <Banner />
+            <Body />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
