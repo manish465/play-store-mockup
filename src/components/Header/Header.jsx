cdb4f87163a4ddb3a72e947fb3d898041bec2191
manish465 <ms5978307@gmail.com>
2020-04-17 16:12:32 -0700

    added a text input feild in top of app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { InputBase, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    inputBase: {
+        background: "#9E9E9E",
+        borderRadius: "21px",
+        width: "1868px",
+        height: "49px",
+        marginTop: "22px",
+        boxShadow: "0 3px 14px",
+    },
+});
 
 const Header = () => {
-    return <div>Header</div>;
+    const classes = useStyles();
+
+    return <InputBase className={classes.inputBase} />;
 };
 
 export default Header;
