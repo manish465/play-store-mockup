bebafc875e789d2e9f87f158b5f640cf8daf995f
manish465 <ms5978307@gmail.com>
2020-04-17 18:56:06 -0700

    added tab bar

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Tabs, Tab, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        color: "white",
+        textTransform: "initial",
+    },
+    indicator: {
+        background: "#00F110",
+    },
+});
 
 const TabBar = () => {
-    return <div>Tab Bar</div>;
+    const [value, setValue] = useState(0);
+    const classes = useStyles();
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Tabs
+            variant='fullWidth'
+            classes={{ root: classes.root, indicator: classes.indicator }}
+            value={value}
+            onChange={handleChange}>
+            <Tab classes={{ root: classes.root }} label='For you' />
+            <Tab classes={{ root: classes.root }} label='Top charts' />
+            <Tab classes={{ root: classes.root }} selected label='Events' />
+            <Tab classes={{ root: classes.root }} label='Premium' />
+            <Tab classes={{ root: classes.root }} label='Categories' />
+            <Tab classes={{ root: classes.root }} label='Family' />
+            <Tab classes={{ root: classes.root }} label='Editors Choice' />
+        </Tabs>
+    );
 };
 
 export default TabBar;
