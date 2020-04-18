2c6e85cbe7594941fb434e4792924f29dbff2640
manish465 <ms5978307@gmail.com>
2020-04-17 22:14:29 -0700

    added Bottom nav bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
-const Footer = () => {
-    return <div>Footer</div>;
-};
+import { makeStyles } from "@material-ui/core/styles";
 
-export default Footer;
+import { BottomNavigationAction, BottomNavigation } from "@material-ui/core";
+
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import AppsIcon from "@material-ui/icons/Apps";
+import LocalMoviesIcon from "@material-ui/icons/LocalMovies";
+import BookIcon from "@material-ui/icons/Book";
+
+const useStyles = makeStyles({
+    root: {
+        background: "inherit",
+        width: "auto",
+        justifyContent: "space-around",
+    },
+    tab: {
+        color: "#00F110",
+    },
+});
+
+export default function LabelBottomNavigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState("games");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={handleChange}
+            className={classes.root}>
+            <BottomNavigationAction
+                label='Games'
+                value='games'
+                classes={{ selected: classes.tab }}
+                icon={<VideogameAssetIcon />}
+            />
+            <BottomNavigationAction
+                label='Apps'
+                value='app'
+                classes={{ selected: classes.tab }}
+                icon={<AppsIcon />}
+            />
+            <BottomNavigationAction
+                label='Movies'
+                value='movies'
+                classes={{ selected: classes.tab }}
+                icon={<LocalMoviesIcon />}
+            />
+            <BottomNavigationAction
+                label='Books'
+                value='books'
+                classes={{ selected: classes.tab }}
+                icon={<BookIcon classes={{ colorAction: classes.tab }} />}
+            />
+        </BottomNavigation>
+    );
+}
