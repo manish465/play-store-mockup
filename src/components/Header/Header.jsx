4bf56477d475329ed0bd769e304302f0d06dff53
manish465 <ms5978307@gmail.com>
2020-04-17 17:24:22 -0700

    added style for text feild

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { InputBase, makeStyles } from "@material-ui/core";
+
+import { InputBase, makeStyles, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles({
-    inputBase: {
+    inputHeader: {
         background: "#9E9E9E",
         borderRadius: "21px",
-        width: "1868px",
         height: "49px",
-        marginTop: "22px",
+        margin: "22px 26px",
         boxShadow: "0 3px 14px",
+        textAlign: "start",
+    },
+    menueIcon: {
+        padding: "11px",
+        marginLeft: "20px",
+        marginRight: "20px",
+        color: "#D1D1D1",
     },
 });
 
 const Header = () => {
     const classes = useStyles();
 
-    return <InputBase className={classes.inputBase} />;
+    return (
+        <div className={classes.inputHeader}>
+            <IconButton className={classes.menueIcon}>
+                <MenuIcon />
+            </IconButton>
+            <InputBase placeholder='Search for apps and games' />
+        </div>
+    );
 };
 
 export default Header;
