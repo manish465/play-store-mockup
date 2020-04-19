78bac4e8ebc141285af2647846402d4f6aa8ab9c
manish465 <ms5978307@gmail.com>
2020-04-18 19:28:21 -0700

    added data and heading

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    content: {
+        marginTop: "97px",
+        marginLeft: "26px",
+    },
+    heading: {
+        color: "#D9D9D9",
+    },
+});
 
 const Body = () => {
-    return <div>Body</div>;
+    const classes = useStyles();
+
+    return (
+        <div className={classes.content}>
+            <Typography className={classes.heading} align='left' variant='h4'>
+                Discover Something new
+            </Typography>
+        </div>
+    );
 };
 
 export default Body;
---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+import img1 from "../assets/Bodydata/img1.png";
+import img2 from "../assets/Bodydata/img2.png";
+import img3 from "../assets/Bodydata/img3.png";
+import img4 from "../assets/Bodydata/img4.png";
+import img5 from "../assets/Bodydata/img5.png";
+import img6 from "../assets/Bodydata/img6.png";
+
+const data = [
+    {
+        id: 1,
+        name: "BATTELFEILD 4",
+        space: 6.43,
+        imgUrl: img1,
+    },
+    {
+        id: 2,
+        name: "BATTELFEILD 2",
+        space: 4.11,
+        imgUrl: img2,
+    },
+    {
+        id: 3,
+        name: "MGS 4",
+        space: 5.67,
+        imgUrl: img3,
+    },
+    {
+        id: 4,
+        name: "MGS:G0",
+        space: 1.91,
+        imgUrl: img4,
+    },
+    {
+        id: 5,
+        name: "MASS EFFECT 3",
+        space: 11.85,
+        imgUrl: img5,
+    },
+    {
+        id: 6,
+        name: "OVERWATCH",
+        space: 22.7,
+        imgUrl: img6,
+    },
+];
+
+export default data;
