15b1abe3e72723662c005b5f1b5f252f5adaedd6
manish465 <ms5978307@gmail.com>
2020-04-18 20:32:45 -0700

    added data to body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,24 +1,35 @@
 import React from "react";
+import data from "../../data/data";
 import { Typography, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
     content: {
-        marginTop: "97px",
+        marginTop: "47px",
         marginLeft: "26px",
     },
     heading: {
         color: "#D9D9D9",
+    },
+    apps: {
+        marginTop: "54px",
     },
 });
 
 const Body = () => {
     const classes = useStyles();
 
+    const cards = data.map((card) => (
+        <div className={classes.apps}>
+            <img src={card.imgUrl} alt='' />
+        </div>
+    ));
+
     return (
         <div className={classes.content}>
             <Typography className={classes.heading} align='left' variant='h4'>
                 Discover Something new
             </Typography>
+            {cards}
         </div>
     );
 };
